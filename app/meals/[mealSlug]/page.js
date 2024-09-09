96a439ca4c37db531ad17ed959b9d8bc60ada5e2
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-09-09 12:42:56 +0200

    [add] Form submission to share meal page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,9 +2,14 @@ import Image from "next/image";
 
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
 	const meal = getMeal(params.mealSlug);
+
+	if (!meal) {
+		notFound();
+	}
 
 	meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
@@ -17,7 +22,10 @@ export default function MealDetailsPage({ params }) {
 				<div className={classes.headerText}>
 					<h1>{meal.title}</h1>
 					<p className={classes.creator}>
-						by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+						by{" "}
+						<a href={`mailto:${meal.creator_email}`} target="_blank">
+							{meal.creator}
+						</a>
 					</p>
 					<p className={classes.summary}>{meal.summary}</p>
 				</div>
